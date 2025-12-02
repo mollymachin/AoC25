5c228278e2d3e65c62a839ceee61106cf52cd5cb
Molly Machin <Open>
2025-12-02 23:43:41 +0000

    Finished solution for part 2.

--- Day2/Day2b.py
+++ Day2/Day2b.py
@@ -3,10 +3,11 @@ import re
 
 def is_invalid(n):
     s = str(n)
-    if len(s) % 2 != 0:
-        return False
-    mid = len(s) // 2
-    return s[:mid] == s[mid:]
+    length = len(s)
+    return any(
+        length % i == 0 and s == s[:i] * (length // i)
+        for i in range(1, length // 2 + 1)
+    )
 
 res = []
 with open("Day2/Day2.txt", "r") as file:
